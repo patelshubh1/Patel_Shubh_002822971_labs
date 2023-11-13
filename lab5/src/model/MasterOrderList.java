886d28c34358a46b792f802c0b82c3e97edbfd23
Shubh Patel <145070010+patelshubh1@users.noreply.github.com>
2023-11-12 19:15:11 -0500

    Add files via upload

---
+++ lab5/src/model/MasterOrderList.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author shubh
+ */
+
+import java.util.ArrayList;
+
+
+public class MasterOrderList {
+    
+    ArrayList<Order> orderList;
+
+    public MasterOrderList() {
+        
+        orderList=new ArrayList<Order>();
+    }
+
+    public ArrayList<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(ArrayList<Order> orderList) {
+        this.orderList = orderList;
+    }
+
+ 
+    public void addNewOrder(Order order)
+    {
+        this.orderList.add(order);
+    }
+    
+}
